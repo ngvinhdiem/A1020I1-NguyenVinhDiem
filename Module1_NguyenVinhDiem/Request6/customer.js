8f76c10102ef0ead3b2645252812b6604a9e7a00
A1020I1-NguyenVinhDiem <ngvinhdiem@gmail.com>
2020-12-28 12:37:21 +0700

    update

---
+++ Module1_NguyenVinhDiem/Request6/customer.js
@@ -0,0 +1,24 @@
+class Customer {
+    setName(name) {this.name=name}
+    getName() {return this.name}
+    setID(idNumber) {this.idNumber=idNumber}
+    getID() {return this.idNumber}
+    setDoB(dateOfBirth) {this.dateOfBirth=dateOfBirth}
+    getDoB() {return this.dateOfBirth}
+    setEmail(email) {this.email=email}
+    getEmail() {return this.email}
+    setAdress(address) {this.address=address}
+    getAdress() {return this.address}
+    setCustomerType(customerType) {this.customeType=customerType}
+    getCustomerType() {return this.customeType}
+    setDiscount(discount) {this.discount=discount}
+    getDiscount() {return this.discount}
+    setExtra(extra) {this.extra=extra}
+    getExtra() {return this.extra}
+    setRentDays(rentDays) {this.rentDays=rentDays}
+    getRentDays() {return this.rentDays}
+    setServiceType(serviceType) {this.serviceType=serviceType}
+    getServiceType() {return this.serviceType}
+    setRoomType(roomType) {this.roomType=roomType}
+    getRoomType() {return this.roomType}
+}
