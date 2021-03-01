30862d00d4bba77a18ce47674841fdc4fb01e888
A1020I1-NguyenVinhDiem <ngvinhdiem@gmail.com>
2021-03-01 19:28:08 +0700

    update

--- module_2/src/b11_dsa_stack_queue/baitap/DemSoLanXuatHienSuDungMap.java
+++ module_2/src/b11_dsa_stack_queue/baitap/DemSoLanXuatHienSuDungMap.java
@@ -1,26 +1,18 @@
 package b11_dsa_stack_queue.baitap;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DemSoLanXuatHienSuDungMap {
     public static void main(String[] args) {
-        Map treeMap = new HashMap();
-        Set set = treeMap.keySet();
-        String mainString = "di hoc code o code gym va go code o intelij";
-        String lowerString, upperString;
+        Map<String,Integer> treeMap = new LinkedHashMap<>();
+        Set<String> set = treeMap.keySet();
+        String mainString = "di hoc code o code gym va hoc go code o intelij";
         for (String word : mainString.split(" ")) {
-            for (Object key : set) {
-                lowerString=word.toLowerCase();
-                upperString=word.toUpperCase();
-                if (lowerString==key||upperString==key) treeMap.put(key, (int) (treeMap.get(key)) + 1);
-                else treeMap.put(word, 1);
-            }
+            if (set.contains(word)) treeMap.put(word, (int) (treeMap.get(word)) + 1);
+            else treeMap.put(word, 1);
         }
 
-
-        for (Object key : set) {
+        for (String key : set) {
             System.out.println(key + " " + treeMap.get(key));
         }
     }
