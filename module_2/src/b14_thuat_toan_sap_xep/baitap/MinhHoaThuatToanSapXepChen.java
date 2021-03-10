d74de25af7cce9daac28af4c908890849284b3d7
A1020I1-NguyenVinhDiem <ngvinhdiem@gmail.com>
2021-03-10 17:33:41 +0700

    update

---
+++ module_2/src/b14_thuat_toan_sap_xep/baitap/MinhHoaThuatToanSapXepChen.java
@@ -0,0 +1,31 @@
+package b14_thuat_toan_sap_xep.baitap;
+
+import java.util.Arrays;
+
+public class MinhHoaThuatToanSapXepChen {
+    public static void main(String[] args) {
+
+        int[] a = {5, 9, 1, 3, 2, 6, 7};
+        insertionSort(a);
+    }
+    public static void insertionSort(int[] list) {
+        for (int i = 1; i < list.length; i++) {
+
+            int curElement = list[i];
+            int j;
+            for (j = i - 1; j >= 0; j--) {
+                if (curElement < list[j]) {
+                    list[j + 1] = list[j];
+                } else {
+                    break;
+                }
+            }
+
+            list[j + 1] = curElement;
+            System.out.println(Arrays.toString(list));
+        }
+
+        System.out.println(Arrays.toString(list));
+    }
+
+}
